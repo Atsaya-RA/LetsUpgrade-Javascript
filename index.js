6a33f219355fed760343ce03c22648d805c01ca2
Atsaya-RA <70716894+Atsaya-RA@users.noreply.github.com>
2020-09-04 17:22:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 //Question 1
-console.log("1.Program to search for a particular character in a string:");
+console.log("1.Program to search for a particular character in a string");
 let sampleword = "Javascript";
 let tofind = 's';
 console.log("The String is: "+sampleword);
-if(sampleword.indexOf(find) >0){
-    console.log("The Charcter searched is "+tofind+" and is found at: "+sampleword.indexOf(tofind+1));
+if(sampleword.indexOf(tofind) >0){
+    console.log("The Character searched is "+tofind+" and is found at: "+(sampleword.indexOf(tofind)+1));
 }
 else{
-    console.log("The Charcter searched is "+tofind+" and it is not found.");
+    console.log("The Character searched is "+tofind+" and it is not found.");
 }
 console.log("-----------------------------------------------------------------");
 
 //Question2 
-console.log("2.Program to convert minutes to secs:");
+console.log("2.Program to convert minutes to secs");
 let timeinmins = 5;
 console.log("Time in Minutes: "+timeinmins);
 console.log("Time in Seconds: "+timeinmins*60);
 console.log("-----------------------------------------------------------------");
 
 //Question 3
-console.log("3.Program to search for an element in an array of strings:");
+console.log("3.Program to search for an element in an array of strings");
 let sampledata =["ODC","Cafeteria","Library","Clinic","Central Spine"];
 let position = 2;
 let tosearch = "Clinic";
-console.log(`The element searched is: ${tosearch} and is found at: ${sampledata.findIndex(tosearch)+1}`);
+console.log("The element searched is: "+tosearch+" and is found at: "+(sampledata.indexOf(tosearch)+1));
 console.log("Element at position "+position+" is: "+sampledata[position-1]);
 console.log("-----------------------------------------------------------------");
 
 //Question 4
-console.log("4.Program to display only elements containg 'a' in an array:");
+console.log("4.Program to display only elements containg 'a' in an array");
 let data =["ODC","Cafeteria","Library","Clinic","Central Spine"];
 console.log("Words containing alphabet 'a' are:");
 data.forEach(function(ele)
 {
 
-	if(ele.indexOf('a')>=0)
+	if((ele.indexOf('a') )>=0)
 	{
-		console.log(a);
+		console.log(ele);
 	}
 });
 console.log("-----------------------------------------------------------------");
 
 //Question 5
+console.log("5.Program to print an array in reverse order");
 let vowels=['a','e','i','o','u'];
 console.log("Reverse order of Array:");
 for(let i=vowels.length;i>0;i--)
